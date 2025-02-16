ac559caa4694b38ba0c07ece63e5d3640c416278
Ivan Zinovyev <ivan@zinovyev.net>
2025-02-16 16:01:48 +0200

    Adding items to the list

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,21 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const existingItem = state.items.find(item => item.name == action.payload.name);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({...action.payload, quantity: 1});
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload.name)
     },
     updateQuantity: (state, action) => {
-
-    
+      const existingItem = state.items.find(item => item.name == action.payload.name);
+      if (existingItem) {
+        existingItem.quantity = action.payload.quantity;
+      }
     },
   },
 });
