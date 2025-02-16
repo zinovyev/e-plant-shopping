45735b097bc52966420fbea1df135db522dc7603
Ivan Zinovyev <ivan@zinovyev.net>
2025-02-16 17:25:59 +0200

    Adding a count of items to the cart icon

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -299,7 +299,7 @@ function ProductList() {
                   <rect width="156" height="156" fill="none"></rect>
                   <circle cx="80" cy="216" r="12"></circle>
                   <circle cx="184" cy="216" r="12"></circle>
-                  <text x="100" y="150" fill="white">{addedToCart.length}</text>
+                  <text x="100" y="150" fill="white">{addedToCart.reduce((acc, item) => (acc + item.quantity), 0)}</text>
                   <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path>
                 </svg>
               </h1>
