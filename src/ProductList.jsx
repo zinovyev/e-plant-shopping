4aa8fe586e342cfef275d01ed3be9263a9d1a3ef
Ivan Zinovyev <ivan@zinovyev.net>
2025-02-16 17:24:08 +0200

    Adding a count of items to the cart icon

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -10,7 +10,6 @@ function ProductList() {
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
   // const [addedToCart, setAddedToCart] = useState({}); // We don't need it anymore since we're using the selector now
   const addedToCart = useSelector(state => state.cart.items)
-  console.log(addedToCart);
 
   const plantsArray = [
     {
@@ -293,7 +292,19 @@ function ProductList() {
         </div>
         <div style={styleObjUl}>
           <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-          <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+          <div>
+            <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+              <h1 className='cart'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                  <rect width="156" height="156" fill="none"></rect>
+                  <circle cx="80" cy="216" r="12"></circle>
+                  <circle cx="184" cy="216" r="12"></circle>
+                  <text x="100" y="150" fill="white">{addedToCart.length}</text>
+                  <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path>
+                </svg>
+              </h1>
+            </a>
+          </div>
         </div>
       </div>
       {!showCart? (
